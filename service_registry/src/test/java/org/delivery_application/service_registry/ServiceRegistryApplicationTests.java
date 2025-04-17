28b588f45daeccfecaaa486046b098298a0f7486
udithgunasekara <supeshalagunasekara@gmail.com>
2025-04-17 15:11:53 +0530

    feat: Eureka Client registration (port: 8761)

---
+++ service_registry/src/test/java/org/delivery_application/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.delivery_application.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
