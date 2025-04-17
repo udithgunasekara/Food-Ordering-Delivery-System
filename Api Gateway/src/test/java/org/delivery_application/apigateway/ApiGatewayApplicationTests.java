7fce9594b1dcc6b778ab70c8e72e73e65e8ebdcb
udithgunasekara <supeshalagunasekara@gmail.com>
2025-04-17 15:53:13 +0530

    feat: add API gateway

---
+++ Api Gateway/src/test/java/org/delivery_application/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.delivery_application.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
